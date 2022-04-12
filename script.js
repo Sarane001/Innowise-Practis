8d39a64e60b42c8499614900e39a90e5e73dbf79
Alexandr <popkov-01@bk.ru>
2022-04-12 21:32:33 +0300

    ActualVersion

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-const link = 
-    "http://api.weatherstack.com/current?access_key=2ee14621147847388a2160703222103";
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=02c241e5f5c19e653240d087fd657644')
+//     .then(function (resp) { return resp.json() } )
+//     .then(function (data) {
+//         console.log(data);
+//         document.querySelector('.city-info__title').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+//         document.querySelector('.city-title').innerHTML = data.name;
+//         document.querySelector('.city-info__subtitlesunrise').innerHTML = (new Date(data.sys.sunrise)).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' PM';
+//         document.querySelector('.city-info__subtitlesunset').innerHTML = (new Date(data.sys.sunset)).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' AM';
+//         document.querySelector('.description').textContent = data.weather[0] ['description'];
+//         document.querySelector('.humidity1').innerHTML = data.main.humidity + '%';
+//         document.querySelector('.pressure2').innerHTML = (data.main.pressure / 1000) + ' mBar';
+//         document.querySelector('.windespeed3').innerHTML = Math.floor(data.wind.speed * 1.6093) + 'km/h';
+//         //document.querySelector('.icon').innerHTML = '<img src = "http://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">';
+//     })
+//     .catch(function () {
+
+//     });
+
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=02c241e5f5c19e653240d087fd657644')
+//     .then(function (resp1) { return resp1.json() } )
+//     .then(function (data1) {
+//       console.log(data1);
+//       document.querySelector('.hour1').textContent = data.current.temp;
+//       // document.querySelector('.hour1').textContent = (new Date(data.dt)).toLocaleTimeString([], {hour: '2-digit'}) + ' AM'+ '\r\n'+ (Math.round(data.main.temp - 273) + "°");
+//       // document.querySelector('.hour2').textContent = (new Date(data.dt)).toLocaleTimeString([], {hour: '2-digit'}) + ' AM'+ '\r\n'+ (Math.round(data.main.temp - 273) + "°");
+//     })
+//     .catch(function () {
+
+//     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const link = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=02c241e5f5c19e653240d087fd657644";
 
     const root = document.getElementById('root');
     const popup = document.getElementById('popup');
@@ -7,81 +49,78 @@ const link =
     const form = document.getElementById('form');
     const popupClose = document.getElementById('close');
 
-
-    let store = {
-        city: "Minsk",
-        feelslike: 0,
-        temperature: 0,
-        observationTime: "00:00 AM",
-        isDay: "yes",
-        weatherDescriptions: "",
+    const store = {
+        name: "London",
+        // temp: 0,
+        main: {
+          temp: 280.32,
+          "pressure": 1012,
+          "humidity": 81,
+          "temp_min": 279.15,
+          "temp_max": 281.15
+        },
+        dt: "00:00 AM",
+        sunset: "06:00 PM",
+        sunrise: "07:00 AM",
+        isDay: "no",
+        description: "partly cloudy",
         properties: {
-            cloudcover: {},
+            all: {},
             humidity: {},
-            windSpeed: {},
+            speed: {},
             visibility: {},
             pressure: {},
-            uvIndex: {},
         },
     };
 
 const fetchData = async () => {
     try {
-        const query = localStorage.getItem('query') || store.city;
-        const result = await fetch(`${link}&query=${query}`);
+        const query = localStorage.getItem('query') || store.name;
+        const result = await fetch(`${link}&q=${query}`);
         const data = await result.json();
 
         const { 
           current: { 
-            feelslike, 
-            cloudcover, 
-            temperature, 
+            all, 
+            temp, 
             humidity,
-            observation_time: observationTime, 
+            dt: dt, 
             pressure, 
-            uv_index: uvIndex, 
             visibility, 
             is_day: isDay, 
-            weather_descriptions: weatherDescriptions,
-            wind_speed: windSpeed,
-
+            description: description,
+            wind_speed: speed,
         }, 
         location: { name },
     } = data;
 
     store = {
         ...store,
-        city: name,
-        feelslike,
-        temperature,
-        observationTime,
+        name: name,
+        temp,
+        dt,
         isDay,
-        weatherDescriptions: weatherDescriptions[0],
+        description: description[0],
         properties: {
-            cloudcover: {
-              title: "cloudcover",
-              value: `${cloudcover}%`,
+          all: {
+              title: "Сloudcover",
+              value: `${all}%`,
               icon: "cloud.png",
             },
             humidity: {
-              title: "humidity",
+              title: "Humidity",
               value: `${humidity}%`,
               icon: "humidity.png",
             },
-            windSpeed: {
+            speed: {
               title: "wind speed",
-              value: `${windSpeed} kmph`,
+              value: `${speed} kmph`,
               icon: "wind.png",
             },
             pressure: {
               title: "pressure",
               value: `${pressure} mb`,
               icon: "gauge.png",
-            },
-            uvIndex: {
-              title: "uv Index",
-              value: `${uvIndex} / 100`,
-              icon: "uv-index.png",
             },
             visibility: {
               title: "visibility",
@@ -97,8 +136,8 @@ const fetchData = async () => {
       }
 };
 
-const getImage = (weatherDescriptions) => {
-    const value = weatherDescriptions.toLowerCase();
+const getImage = (description) => {
+    const value = description.toLowerCase();
   
     switch (value) {
       case "partly cloudy":
@@ -120,21 +159,11 @@ const renderProperty = (properties) => {
     return Object.values(properties).map((data) => {
         const {title, value, icon} = data;
 
-        return `<div class="property">
-                <div class="property-icon">
-                    <img src="./img/icons/${icon}" alt="">
-                </div>
-                <div class="property-info">
-                    <div class="property-info__value">${value}</div>
-                    <div class="property-info__description">${title}</div>
-                </div>
-            </div>`;
-    })
-    .join("");
+        return })
 };
 
 const markup = () => {
-    const {city, weatherDescriptions, observationTime, temperature, isDay, properties } = store;
+    const {name, description, dt, temp, isDay, properties, sunset, sunrise } = store;
     const containerClass = isDay === "yes" ? "is-day" : "is-night";
 
     return `<div class="container ${containerClass}">
@@ -142,22 +171,21 @@ const markup = () => {
                 <div class="city">
                   <div class="city-subtitle">Weather Today in</div>
                     <div class="city-title" id="city">
-                    <span>${city}</span>
+                    <span>${name}</span>
                   </div>
                 </div>
                 <div class="city-info">
                     <div class="top-left">
-                    <img class="icon" src="./img/${getImage(weatherDescriptions)}" alt="" />
-                    <div class="description">${weatherDescriptions}</div>
+                    <img class="icon" src="./img/${getImage(description)}" alt="" />
+                    <div class="description">${description}</div>
                 </div>
                 
                 <div class="top-right">
-                    <div class="city-info__subtitle">as of ${observationTime}</div>
-                    <div class="city-info__title">${temperature}°C</div>
+                    <div class="city-info__title">${temp}°</div>
+                    <div class="city-info__subtitlesunset">${sunset}</div>
+                    <div class="city-info__subtitlesunrise">${sunrise}</div>
                 </div>
                 </div>
-            </div>
-            <div id="properties">${renderProperty(properties)}</div>
             </div>`;
 };
 
@@ -172,20 +200,20 @@ popupClose.addEventListener('click', () => {     // Закрытие выбор�
     popup.classList.toggle("active");
 });
 
-const city = document.getElementById("city");
-city.addEventListener("click", togglePopupClass);
+const name = document.getElementById("favoriteicone");
+name.addEventListener("click", togglePopupClass);
 };
 
 const handleInput = (e) => {
     store = {
         ...store,
-        city: e.target.value,
+        name: e.target.value,
     };
 };
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    const value = store.city
+    const value = store.name
 
     if(!value) return null;
 
@@ -199,3 +227,4 @@ form.addEventListener('submit', handleSubmit);
 textInput.addEventListener('input', handleInput);
 
 fetchData();
+renderComponent();
